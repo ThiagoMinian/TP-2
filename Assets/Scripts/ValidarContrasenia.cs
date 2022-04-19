8d33ea03faa8747060159be74ac7827cf7f70dee
ThiagoMinian <101407767+ThiagoMinian@users.noreply.github.com>
2022-04-19 12:07:40 -0300

    ValidarContraseña

---
+++ Assets/Scripts/ValidarContrasenia.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ValidarContrasenia : MonoBehaviour
+{
+    string contraseniaCorrecta;
+    string contraseniaUsuario;
+    Text ingresoUsuario;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        contraseniaCorrecta = "12345";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void validarContrasenia()
+    {
+        contraseniaCorrecta = ingresoUsuario.text;
+        if (contraseniaUsuario == contraseniaCorrecta)
+        {
+
+        }
+
+    }
+}
